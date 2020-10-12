4d3e07d145fa9fb8e7d23a76edd60df6bfcd42c5
dylanell <dellio15@gmail.com>
2020-10-11 21:04:38 -0400

    analyzing adversarial examples in notebook and added helper plotting function to data utils

--- util/data_utils.py
+++ util/data_utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import imageio
 import glob
+import matplotlib.pyplot as plt
 
 def generate_df_from_image_dataset(path):
     # read train/test label files to dataframe
@@ -73,3 +74,40 @@ def tile_images(imgs):
         tile_imgs[n_idx:(n_idx+h_dim), m_idx:(m_idx+w_dim), :] = imgs[t_idx]
 
     return tile_imgs
+
+def graph_inputs_with_predictions(
+    inputs, labels, output_probs, output_preds, figsize=(8, 10)):
+
+    # remap inputs to [0 - 255] and convetr to uint 8
+    new_min, new_max = 0, 255
+    min, max = np.min(inputs), np.max(inputs)
+    inputs = (((inputs - min) / (max - min)) * (new_max - new_min)) + new_min
+    inputs = inputs.astype(np.uint8)
+
+    # count number of rows
+    num_rows = inputs.shape[0]
+
+    # make a figure to hold multiple plots
+    fig, axs = plt.subplots(num_rows, 2, figsize=figsize)
+
+    for i in range(inputs.shape[0]):
+
+        # save current image to file
+        img = inputs[i, :, :, 0]
+
+        # add image trace
+        axs[i, 0].imshow(img)
+        axs[i, 0].set_title('Target: {}, Prediction: {}'.format(labels[i], output_preds[i]))
+        axs[i, 0].axis('off')
+
+        axs[i, 1].bar(x=range(len(output_probs[i])), height=output_probs[i])
+        axs[i, 1].set_title('Prediction Probabilities')
+        axs[i, 1].set_xticks(range(len(output_probs[i])))
+        axs[i, 1].grid('major')
+        axs[i, 1].set_ylim(0., 1.1)
+        axs[i, 1].set_xlabel('Class')
+        axs[i, 1].set_ylabel('Probability')
+
+    fig.tight_layout()
+
+    return fig
