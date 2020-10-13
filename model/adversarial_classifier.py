1197ed9ecd8b36a2e8cbee4bc94cc530258d3215
dylanell <dellio15@gmail.com>
2020-10-13 11:01:31 -0400

    changed adversarial strength

--- model/adversarial_classifier.py
+++ model/adversarial_classifier.py
@@ -79,7 +79,7 @@ class AdversarialClassifier():
                 adv_loss = self.loss_fn(adv_logits_batch, label_batch)
                 adv_loss.backward()
                 adv_grads = input_batch.grad
-                input_batch = input_batch + (0.3 * torch.sign(adv_grads))
+                input_batch = input_batch + (0.2 * torch.sign(adv_grads))
 
                 # keep pixel values in [-1, 1]
                 new_min, new_max = -1., 1.
