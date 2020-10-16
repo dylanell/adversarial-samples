385d542c39bc96f594b8417db416af5dd6700e8c
dylanell <dellio15@gmail.com>
2020-10-15 20:16:59 -0400

    added function for computing sillhouette coeff cluster metric

--- util/pytorch_utils.py
+++ util/pytorch_utils.py
@@ -37,3 +37,114 @@ def build_image_dataset(
     )
 
     return dataset, dataloader
+
+def sillhouette_coefficient(x, y):
+    '''
+    Computing the Silhouette Coefficient of hidden
+    representations.
+    Reference: https://scikit-learn.org/stable/modules/clustering.html#clustering-performance-evaluation
+    '''
+
+    # get unique labels present in this batch
+    unique_labels, unique_counts = torch.unique(
+        y, return_counts=True)
+
+    # remove unique labels where we only have one class sample
+    unique_labels = unique_labels[unique_counts > 1]
+
+    '''
+    a: The mean distance between a sample and all other
+    points in the same class. Calling this 'mean_intra_class_dist'.
+    '''
+
+    # construct pairwise euclidean distance matrix of
+    # latent representations
+    n = x.shape[0]
+    d = x.shape[1]
+    a = x.unsqueeze(1).expand(n, n, d)
+    b = x.unsqueeze(0).expand(n, n, d)
+    x_dist_matrix = torch.pow(a - b, 2).sum(dim=2)
+    #print(x_dist_matrix)
+
+    # extract pairwise distances only for members of the
+    # same class for each unique class in the label batch
+    # and only if there are more than 1 instance of a class
+    class_wise_x_dist_matrices = [
+        x_dist_matrix[y == l][:, y == l] \
+        for j, l in enumerate(unique_labels)]
+    #print(class_wise_x_dist_matrices[0])
+
+    # grab only the upper traingular values of each
+    # class-wise representation distance matrix to compute
+    # the mean distance between all different samples of
+    # the same class
+    mean_class_wise_rep_dists = torch.cat([
+        torch.mean(class_wise_x_dist_matrices[j][torch.triu( \
+        torch.ones(class_wise_x_dist_matrices[j].shape[0], \
+        class_wise_x_dist_matrices[j].shape[0]), \
+        diagonal=1) == 1]).expand(1) \
+        for j, l in enumerate(unique_labels)], dim=0)
+    #print(mean_class_wise_rep_dists)
+
+    # compute the total mean class-wise representation
+    # distance accross all classes
+    mean_intra_class_dist = torch.mean(mean_class_wise_rep_dists)
+    #print(mean_intra_class_dist)
+
+    '''
+    b: The mean distance between a sample and all other points in
+    the next nearest cluster. Calling this 'mean_inter_class_dist'.
+    '''
+
+    # compute average class-wise feature representations for each
+    # unique label in the current label batch (class clusters)
+    class_clusters = torch.cat([
+        torch.mean(x[y == l], dim=0, \
+        keepdim=True) for j, l in enumerate(unique_labels)], dim=0)
+    #print(class_clusters)
+
+    # compute pairwise distances between each class cluster
+    n = class_clusters.shape[0]
+    d = class_clusters.shape[1]
+    a = class_clusters.unsqueeze(1).expand(n, n, d)
+    b = class_clusters.unsqueeze(0).expand(n, n, d)
+    class_cluster_dist_matrix = torch.pow(a - b, 2).sum(dim=2)
+    #print(class_cluster_dist_matrix)
+
+    # get nearest clusters
+    nearest_cluster = torch.argsort(class_cluster_dist_matrix, dim=1)[:, 1]
+    #print(nearest_cluster)
+
+    # get distance between each point in cluster i, to each point
+    # in cluster nearest to i, and compute mean of these distances
+    # for each cluster, i.
+    inter_dists = []
+    for j, l in enumerate(unique_labels):
+        test_points = x[y == l]
+        nearest_points = x[
+            y == unique_labels[nearest_cluster[j]]]
+        n = test_points.shape[0]
+        m = nearest_points.shape[0]
+        d = test_points.shape[1]
+        a = test_points.unsqueeze(-1).repeat(1, 1, m)
+        b = torch.transpose(
+            nearest_points.unsqueeze(-1).repeat(1, 1, n),
+            dim0=2, dim1=0)
+        test_nearest_dist_matrix = torch.pow(a - b, 2).sum(dim=1)
+        inter_dists.append(torch.mean(test_nearest_dist_matrix).expand(1))
+
+    # compute mean of mean nearest inter class distances for each
+    # cluster, i
+    mean_inter_class_dist = torch.mean(torch.cat(inter_dists, dim=0))
+    #print(mean_inter_class_dist)
+
+    '''
+    s: Compute the Silhouette Coefficient of latent space
+    representations given class assignments from labels.
+    '''
+
+    # sillhouette coeff
+    s = (mean_inter_class_dist - mean_intra_class_dist) \
+        / torch.max(mean_intra_class_dist, mean_inter_class_dist)
+
+    return s
